3667d1c58eb8343d45cf9a58b81cfe82d9985401
lwenhao <1006000545@qq.com>
2023-08-31 23:58:22 +0800

    branch02

--- src/com/lwh/git/demo01.java
+++ src/com/lwh/git/demo01.java
@@ -5,5 +5,6 @@ public class demo01 {
 
     public static void main(String[] args) {
         System.out.println("git demo01 test idea");
+        System.out.println("git demo01 test branch02");
     }
 }
