9ab2ae00a504e0c648a420556d6724256cc393bf
lwenhao <1006000545@qq.com>
2023-08-31 23:52:39 +0800

    测试idea集成git04

--- src/com/lwh/git/demo01.java
+++ src/com/lwh/git/demo01.java
@@ -7,5 +7,6 @@ public class demo01 {
         System.out.println("git demo01 test idea");
         System.out.println("git demo01 test idea1");
         System.out.println("git demo01 test idea2");
+        System.out.println("git demo01 test idea3");
     }
 }
