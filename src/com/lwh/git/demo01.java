3749186646fe52f018aa9b1552e6768ecc5a5461
lwenhao <1006000545@qq.com>
2023-08-31 23:55:36 +0800

    Revert "测试idea集成git02"
    This reverts commit 33f9b524

--- src/com/lwh/git/demo01.java
+++ src/com/lwh/git/demo01.java
@@ -5,8 +5,5 @@ public class demo01 {
 
     public static void main(String[] args) {
         System.out.println("git demo01 test idea");
-        System.out.println("git demo01 test idea1");
-        System.out.println("git demo01 test idea2");
-        System.out.println("git demo01 test idea3");
     }
 }
