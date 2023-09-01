623f6a1506d2bec81174b2bfdcdac436d47c372b
lwenhao <1006000545@qq.com>
2023-09-01 09:56:05 +0800

    测试idea推送工程到github中

--- src/com/lwh/git/demo01.java
+++ src/com/lwh/git/demo01.java
@@ -6,5 +6,6 @@ public class demo01 {
     public static void main(String[] args) {
         System.out.println("git demo01 test idea");
         System.out.println("git demo01 test branch02");
+        System.out.println("测试idea推送工程到github中");
     }
 }
